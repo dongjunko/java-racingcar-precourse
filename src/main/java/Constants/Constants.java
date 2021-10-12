9744280cd6e55b994f619eb0e223c7e22b57b6f4
rhehdwns87 <rhehdwns87@gmail.com>
2021-10-12 23:07:57 +0900

    feat: 상수값 정의

---
+++ src/main/java/Constants/Constants.java
@@ -0,0 +1,9 @@
+package Constants;
+
+public class Constants {
+    public static final String EXCEPTION_MESSAGE_HEADER = "[ERROR]";
+    public static final String EXCEPTION_MESSAGE_CARNAMELENGTH = "자동차 이름은 5자 이하만 가능합니다.";
+    public static final String EXCEPTION_MESSAGE_CARLENGTH = "2개 이상의 자동차 이름을 입력하셔야합니다.";
+    public static final String MESSAGE_ENTERTHECARNAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String MESSAGE_ENTERTHEGAMECOUNT = "시도할 회수는 몇회인가요?";
+}
